e333d158408bbd52f26f0d120ea8360b7a66fef6
Sonya Slegers <sonjay08@gmail.com>
2018-05-10 16:34:12 -0500

    final shopping cart

--- michael-sonya_shopping-cart/index.js
+++ michael-sonya_shopping-cart/index.js
@@ -1,22 +1,51 @@
 'use strict';
 /* eslint-env jquery */
 // enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+
+function generateLiHtml(item) {
+  return `
+<li>
+<span class="shopping-item">${item}</span>
+<div class="shopping-item-controls">
+  <button class="shopping-item-toggle">
+    <span class="button-label">check</span>
+  </button>
+  <button class="shopping-item-delete">
+    <span class="button-label">delete</span>
+  </button>
+</div>
+</li>`;
+}
 function handleSubmit () { 
   $('#js-shopping-list-form').on('submit', event => {
     event.preventDefault();
     const item = $('.js-shopping-list-entry').val();
-    console.log(item);
+  
+    $('ul.shopping-list').append(generateLiHtml(item));
+
   });
-
-  // check and uncheck items on the list by clicking the "Check" button
-
-
-
-  // permanently remove items from the list
-
 }
+
+function toggleCheck() {
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+}
+
+function removeItem() {
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove('li');
+  });
+}
+
+
+// permanently remove items from the list
+
+
 
 function main(){
   handleSubmit();
+  toggleCheck();
+  removeItem();
 }
 $(main);
